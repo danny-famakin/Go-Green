91ae0c2666d36f6cef7e45e4f1bdebcf2116aeac
Melissa Perez <melissaperez@fb.com>
2017-07-12 16:15:25 -0700

    Data can be stored

--- app/src/main/java/com/codepath/gogreen/ReuseActivity.java
+++ app/src/main/java/com/codepath/gogreen/ReuseActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.gogreen;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ public class ReuseActivity extends AppCompatActivity {
     int REQUEST = 1;
     TextView totalBags;
     int total;
+    SharedPreferences storedBags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,12 @@ public class ReuseActivity extends AppCompatActivity {
                 startActivityForResult(i, REQUEST);
             }
         });
+
+        storedBags = getSharedPreferences("bags", 0);
+        total = storedBags.getInt("bagCount", 0);
+        totalBags = (TextView) findViewById(R.id.totalBags);
+        totalBags.setText(String.valueOf(total));
+
     }
         // Receives results from PointsCounter
         // Changes the value from the amount of total bags
@@ -38,13 +46,26 @@ public class ReuseActivity extends AppCompatActivity {
             if (requestCode == REQUEST) {
                 if (resultCode == RESULT_OK) {
                     Log.d("result", "received");
-                    totalBags = (TextView) findViewById(R.id.totalBags);
                     total += i.getIntExtra("bags", 1);
                     totalBags.setText(String.valueOf(total));
+
                 }
             }
         }
 
+        @Override
+        protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences storedBags = getSharedPreferences("bags", 0);
+        SharedPreferences.Editor editor = storedBags.edit();
+        editor.putInt("bagCount",total);
+
+        // Commit the edits!
+        editor.commit();
+    }
         //http://oceancrusaders.org/plastic-crusades/plastic-statistics/
         //One of the biggest issues in our environment is plastic marine debris.
         //Every year around 100,000 marine creatures year because of it.
