8fdd1acdd87973a3981f2df94117ec1213579f43
Anya Zhang <anyazhang@MacBook-Pro-60.dhcp.thefacebook.com>
2017-07-13 13:54:38 -0700

    added feed activity, fragments, menu directory

---
+++ app/src/main/java/com/codepath/gogreen/FeedFragment.java
@@ -0,0 +1,18 @@
+package com.codepath.gogreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by anyazhang on 7/13/17.
+ */
+
+public class FeedFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
