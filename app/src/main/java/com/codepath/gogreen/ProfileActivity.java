f3868c3d81ef48324f4b61f78868de8a4d1e6107
Melissa Perez <melissaperez@fb.com>
2017-08-04 12:14:18 -0700

    Stacked graph updates everytime an action is logged

--- app/src/main/java/com/codepath/gogreen/ProfileActivity.java
+++ app/src/main/java/com/codepath/gogreen/ProfileActivity.java
@@ -122,7 +122,7 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
         try {
-            barChart(daysCounter);
+            barChart(daysCounter + 1);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -285,14 +285,14 @@ public class ProfileActivity extends AppCompatActivity {
             // start of today
 
 
-            cal.add(Calendar.DAY_OF_MONTH, -counter + i );
+            cal.add(Calendar.DAY_OF_MONTH, -counter + i + 1 );
             Date initialDate = cal.getTime();
-            Log.d("hope", String.valueOf(initialDate));
+            Log.d("hopee", String.valueOf(initialDate));
 
             cal.add(Calendar.DAY_OF_MONTH, 1);
             Date finalDate = cal.getTime();
 
-           Log.d("desperate", String.valueOf(finalDate));
+           Log.d("desperatee", String.valueOf(finalDate));
 
             ParseQuery<ParseObject> query = ParseQuery.getQuery("Action");
             query.whereEqualTo("uid", currentUser.getString("fbId"));
@@ -325,7 +325,7 @@ public class ProfileActivity extends AppCompatActivity {
 
                 }
                 onLoadData(i);
-                cal.add(Calendar.DAY_OF_MONTH, counter - i - 1);
+                cal.add(Calendar.DAY_OF_MONTH, counter - i - 2);
 
             } catch (ParseException e) {
                 e.printStackTrace();
